3617edcf603ad2c3152c4c62a6557d5c59dba213
YT-MrXT <canalmrxt@gmail.com>
2026-01-19 21:45:29 +0000

    Update and rename README.md to Source.lua

--- README.md
+++
@@ -1,1 +0,0 @@
-# MrXT-Hub
---
+++ Source.lua
@@ -0,0 +1,7 @@
+game:GetService("StarterGui"):SetCore("SendNotification", { 
+    Title = "MrXT";
+    Text = "BY MrXT";
+    Icon = "rbxthumb://type=Asset&id=96866982801235&w=150&h=150";
+    Duration = 5;
+})
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/Rizeniii/dex/refs/heads/main/CatHub"))()
